bdc8b118ced08d110dda13c8b08bd0f49c1ff9ef
Reheight <dustinpalmatier@icloud.com>
2020-08-29 14:51:52 -0400

    --- Auto-Git Commit ---

--- commands/about.js
+++ commands/about.js
@@ -4,24 +4,42 @@ const { steamKey } = require('../config.json');
 module.exports = {
     "name": "about",
     "description": "About Page.",
+    "alias": ["botstats"],
     async execute(author, message, args, client) {
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(client.user.tag, client.user.avatarURL())
-            .setTitle("**Rust Statistics Information**")
+        const promises = [
+			client.shard.fetchClientValues('guilds.cache.size'),
+            client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)'),
+		];
+
+		return Promise.all(promises)
+			.then(results => {
+				const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+                const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+                
+                const embed = new Discord.MessageEmbed()
+                .setTitle("**Rust Statistics Information**")
                 .setDescription(
-                    `\`rs!\` - Prefix
-                    \`rs!stats <Steam ID/Link>\` - View Rust Statistics
-                    \`rs!statistics <Steam ID/Link>\` - View statistics
-                    \`rs!kd <Steam ID/Link>\` - View Rust KDR
-                    \`rs!kills <Steam ID/Link>\` - View Kills
-                    \`rs!deaths <Steam ID/Link>\` - View Deaths
-                    \`rs!profile <Steam ID/Link>\` - View Steam Profile
-                    \`rs!suggestion <Message>\` - Suggest Something to the Developers
-                    \`rs!botstats\` - View Bot Statistics`
+                    `View your Rust Statistics over the course of your Rust history, we display most of the statistics that could possibly matter to you, we do plan on expanding to other games!`
                 )
+                .addFields(
+                    { name: "<:developer:749333179210661899> Developer", value: `Reheight#4947`, inline: true },
+                    { name: "<:version:749333179315519578> Version", value: `1.0.0`, inline: true },
+                    { name: "<:javascript:749333179130708018> Language", value: `JavaScript`, inline: true },
+                    { name: "<:discordjs:749333179109736488> Library", value: `Discord.JS`, inline: true },
+                    { name: "<:steam:744961989817925767> API", value: `Steam API`, inline: true },
+                    { name: "🏠 Guilds", value: `${totalGuilds}`, inline: true },
+                    { name: "🙍‍♂️ Members", value: `${totalMembers}`, inline: true }
+                )
+                .setThumbnail(client.user.avatarURL())
                 .setTimestamp()
                 .setFooter('Reheight#4947')
                 .setColor(`#ce422b`)
+
+				return message.channel.send(embed);
+			})
+			.catch(console.error);
+        const embed = new Discord.MessageEmbed()
+            
 
         return message.channel.send(embed);
     }
--- commands/suggestion.js
+++ commands/suggestion.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
     "name": "suggestion",
     "description": "Suggest the owner something.",
+    "aliases": [],
     async execute(author, message, args, client) {
         const suggestion = args.join(" ");
         if (args.length <= 0 || suggestion.length < 20) {
